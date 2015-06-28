0146fa57d9b0c1589e2bee5d4c4660d5fbddb4f6
Devvrat Arya <devvarya@gmail.com>
2015-06-28 18:14:44 +0530

    4 plots

--- plot4.R
+++ plot4.R
@@ -1,19 +1,37 @@
+#Load the text file
+
 df <- "./household_power_consumption.txt"
+
+#Read the text file
+
 data <- read.table(df, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
-voltage <- as.numeric(subSetData$Voltage)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+#Subset the data appropriately
 
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Convert date and time variable 
+
+datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+#Coerce to numeric 
+
+globalActivePower <- as.numeric(subdata$Global_active_power)
+globalReactivePower <- as.numeric(subdata$Global_reactive_power)
+voltage <- as.numeric(subdata$Voltage)
+subMetering1 <- as.numeric(subdata$Sub_metering_1)
+subMetering2 <- as.numeric(subdata$Sub_metering_2)
+subMetering3 <- as.numeric(subdata$Sub_metering_3)
+
+#Launch the graphics device(file device:PNG)
 
 png("plot4.png", width=480, height=480)
+
+#Specifying global graphics parameter(here: the no. of plots per row,column)
+
 par(mfrow = c(2, 2)) 
+
+#Create(and annotate) the plots
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
@@ -25,5 +43,7 @@ lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+#Close the graphics device
 
 dev.off()
