78fcaa8c589712b40f886b0262d85f920a2292b9
Devvrat Arya <devvarya@gmail.com>
2015-06-28 18:14:44 +0530

    Energy Sub Metering

--- plot3.R
+++ plot3.R
@@ -1,17 +1,37 @@
-df <- "./household_power_consumption.txt"
-data <- read.table(df, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+#Load the text file
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+df <- "./household_power_consumption.txt"
+
+#Read the text file
+
+data <- read.table(df, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Subset the data appropriately
+
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Convert date and time variable 
+
+datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+#Coerce to numeric 
+
+globalActivePower <- as.numeric(subdata$Global_active_power)
+subMetering1 <- as.numeric(subdata$Sub_metering_1)
+subMetering2 <- as.numeric(subdata$Sub_metering_2)
+subMetering3 <- as.numeric(subdata$Sub_metering_3)
+
+#Launch the graphics device(file device:PNG)
 
 png("plot3.png", width=480, height=480)
+
+#Create(and annotate) the plot
+
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+#Close the graphics device
+
 dev.off()
